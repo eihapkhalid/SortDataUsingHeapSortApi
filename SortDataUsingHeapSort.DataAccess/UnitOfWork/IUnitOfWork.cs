7b2c23793c3a79bbd0471904ef5ead0be3f9cae4
eihapKhalid <eihapinfo@gmail.com>
2023-11-29 11:05:50 +0300

    Create UnitOfWork design pattern

---
+++ SortDataUsingHeapSort.DataAccess/UnitOfWork/IUnitOfWork.cs
@@ -0,0 +1,17 @@
+﻿using SortDataUsingHeapSort.DataAccess.Repository.IRepository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SortDataUsingHeapSort.DataAccess.UnitOfWork
+{
+    public interface IUnitOfWork
+    {
+        void Save();
+
+        //public ITbRepository Tb { get; }
+        public ISensorDataSampleRepository TbSensorDataSampleRepository { get; }
+    }
+}
---
+++ SortDataUsingHeapSort.DataAccess/UnitOfWork/UnitOfWork.cs
@@ -0,0 +1,53 @@
+﻿using SortDataUsingHeapSort.DataAccess.Data;
+using SortDataUsingHeapSort.DataAccess.Repository;
+using SortDataUsingHeapSort.DataAccess.Repository.IRepository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SortDataUsingHeapSort.DataAccess.UnitOfWork
+{
+    public class UnitOfWork
+    {
+        #region dependency injection
+        private HeapSortDbContext _db;
+
+        // public ITbRepository Tb { get; private set; }
+        public ISensorDataSampleRepository TbSensorDataSampleRepository { get; private set; }
+
+        public UnitOfWork(HeapSortDbContext db)
+        {
+            _db = db;
+            //Tb = new TbRepository(_db);
+            TbSensorDataSampleRepository = new SensorDataSampleRepository(_db);
+
+        }
+        #endregion
+
+        #region Save
+        public void Save()
+        {
+            try
+            {
+                int savedChanges = _db.SaveChanges();
+                if (savedChanges > 0)
+                {
+                    Console.WriteLine("Data saved successfully. Number of affected rows: " + savedChanges);
+
+                }
+                else
+                {
+                    Console.WriteLine("No data changes to save.");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("An error occurred while saving data to the database: " + ex.Message);
+                // يمكنك التعامل مع الاستثناء هنا، مثلاً طباعة رسالة الخطأ أو تسجيلها
+            }
+        }
+        #endregion
+    }
+}
