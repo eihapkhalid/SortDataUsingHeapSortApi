a3fe4d177b554bf2bbc7c7e81aeee009377e19e0
eihapKhalid <eihapinfo@gmail.com>
2023-11-29 10:43:46 +0300

    Create TbSensorDataSample

---
+++ SortDataUsingHeapSort.Models/TbSensorDataSample.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SortDataUsingHeapSort.Models
+{
+    public class TbSensorDataSample
+    {
+        [Key]
+        public int SensorDataId { get; set; }
+
+        [Required(ErrorMessage = "يجب إدخال الاسم")]
+        [StringLength(100, ErrorMessage = "يجب ألا يتجاوز الاسم 100 حرف")]
+        public string SensorDataName { get; set; }
+
+        [Required(ErrorMessage = "يجب إدخال الراتب")]
+        public float SensorDataValue { get; set; }
+
+    }
+}
