818d1dbfb01d0cc9da8e03333c73e09e2378a668
eihapKhalid <eihapinfo@gmail.com>
2023-11-29 11:34:25 +0300

    Create HeapSortsController

---
+++ SortDataUsingHeapSortApi/Controllers/HeapSortsController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SortDataUsingHeapSort.DataAccess.UnitOfWork;
+using SortDataUsingHeapSortApi.Utilities;
+
+namespace SortDataUsingHeapSortApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HeapSortsController : ControllerBase
+    {
+        #region Inject
+        private readonly IUnitOfWork _unitOfWork;
+        private SortDataService _SortDataService;
+
+        public HeapSortsController(IUnitOfWork unitOfWork, SortDataService SortDataService)
+        {
+            _unitOfWork = unitOfWork;
+            _SortDataService = SortDataService;
+        }
+        #endregion
+
+        #region Sort Data By Heap Sorting
+        [HttpGet("SortEmployeesByAge")]
+        public IActionResult SortData()
+        {
+            var data = _unitOfWork.TbSensorDataSampleRepository.GetAll().ToList();
+            _SortDataService.HeapSort(data);
+
+            // إرجاع استجابة مناسبة
+            return Ok(data);
+        }
+        #endregion
+    }
+}
