b72aa4e3b998c78340b7d3d4e95cc0c87537514b
eihapKhalid <eihapinfo@gmail.com>
2023-11-29 10:46:34 +0300

    Create DbContext file

---
+++ SortDataUsingHeapSort.DataAccess/Data/HeapSortDbContext.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SortDataUsingHeapSort.Models;
+
+namespace SortDataUsingHeapSort.DataAccess.Data
+{
+    public class HeapSortDbContext : DbContext
+    {
+        public HeapSortDbContext(DbContextOptions<HeapSortDbContext> options)
+            : base(options)
+        {
+        }
+
+        // public DbSet<TbProduct> TbProducts { get; set; }
+        public DbSet<TbSensorDataSample> TbSensorDataSamples { get; set; }
+
+
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+        }
+    }
+}
